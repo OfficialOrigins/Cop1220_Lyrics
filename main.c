7afbd263f1e960e94c80aa21ce51c2eb641c9983
OfficialOrigins <138532627+OfficialOrigins@users.noreply.github.com>
2025-01-20 04:09:40 +0000

    fixed

--- main.c
+++ main.c
@@ -5,23 +5,23 @@
 int main(void){
 
   //Unit test 1
-  // printf("oh, well imagine /\n");
-  // printf("As I'm pacing the pews in a church corridor /\n");
-  // printf("And I can't help but to hear /\n");
-  // printf("No, I can't help but to hear an exchanging of words...\n");
+  printf("oh, well imagine /\n");
+  printf("As I'm pacing the pews in a church corridor /\n");
+  printf("And I can't help but to hear /\n");
+  printf("No, I can't help but to hear an exchanging of words...\n");
 
   //Unit test 2 
-  // printf("Oh-oh-oh-oh-oh,\n");
-  // printf("oh-oh-oh-oh,\n");
-  // printf("oh-oh-oh /\n");
-  // printf("Caught in a bad romance...\n");\
+  printf("Oh-oh-oh-oh-oh,\n");
+  printf("oh-oh-oh-oh,\n");
+  printf("oh-oh-oh /\n");
+  printf("Caught in a bad romance...\n");\
 
   //Unit test 3
-  // printf("Coming out of my cage\n"); 
-  // printf("And I've been doing just fine\n");
-  // printf("Gotta gotta be down\n");
-  // printf("Because I want it all\n");
-  // return 0;
+  printf("Coming out of my cage\n"); 
+  printf("And I've been doing just fine\n");
+  printf("Gotta gotta be down\n");
+  printf("Because I want it all\n");
+  return 0;
 
   //Unit test 4
   printf("Now, this is a story all about how");
